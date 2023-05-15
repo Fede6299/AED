77387cfafb6b80d269c5c8e836ae558c9196b310
Fede6299 <fapaza@frba.utn.edu.ar>
2023-05-15 17:53:20 -0300

    clase del 27/04

---
+++ 01-CppTiposDeDatos/main.cpp
@@ -0,0 +1,27 @@
+/*  Tipos de Datos
+    FHA
+    20230427
+ */
+
+#include <iostream>
+#include <cassert>
+
+int main()
+{
+    //bool
+    assert (     not true == false      );
+    assert (     not true != true      );
+    assert (true and false == false); //assert sirve para comprobar cosas
+    assert (false or false == false); 
+    assert (false or true == true); 
+    
+    //Entender como funcionan:
+    //char
+    assert('a'+1=='b');
+    assert('c'-'a'==2); //distancia entre a y c
+    assert('@'+1=='A');
+    //unsigned
+    //int
+    //double
+    //string
+}
