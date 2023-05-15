aeaec044cbcb155d93e1819357b839d93dc6e767
Fede6299 <fapaza@frba.utn.edu.ar>
2023-05-15 17:59:14 -0300

    formato

--- 01-CppTiposDeDatos/main.cpp
+++ 01-CppTiposDeDatos/main.cpp
@@ -8,20 +8,20 @@
 
 int main()
 {
-    //bool
-    assert (     not true == false      );
-    assert (     not true != true      );
-    assert (true and false == false); //assert sirve para comprobar cosas
-    assert (false or false == false); 
-    assert (false or true == true); 
-    
-    //Entender como funcionan:
-    //char
-    assert('a'+1=='b');
-    assert('c'-'a'==2); //distancia entre a y c
-    assert('@'+1=='A');
-    //unsigned
-    //int
-    //double
-    //string
+    // bool
+    assert(not true == false);
+    assert(not true != true);
+    assert(true and false == false); // assert sirve para comprobar cosas
+    assert(false or false == false);
+    assert(false or true == true);
+
+    // Entender como funcionan:
+    // char
+    assert('a' + 1 == 'b');
+    assert('c' - 'a' == 2); // distancia entre a y c
+    assert('@' + 1 == 'A');
+    // unsigned
+    // int
+    // double
+    // string
 }
